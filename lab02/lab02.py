250c9f161432d10c63b24318cec8f20410927f1c
PinkHappyBear <31932838+PinkHappyBear@users.noreply.github.com>
2017-10-22 20:32:16 +0300

    lab02 v2

--- lab02/lab02.py
+++ lab02/lab02.py
@@ -1,23 +1,13 @@
 import math
 from math import atan
 
-dataa = input("Введите a:")
-datab =input("Введите b:")
-if dataa and datab:
-    try:
-        a = float(dataa)
-        b = float(datab)
-        print(a,b)
-    except ValueError:
-        print('Нужно было вводить число,но так как циклы мы ещё не проходили,break НЕ БУДЕТ,программа просто крашнется')
+a = int(input("Введите a:"))
+b =int(input("Введите b:"))
+if a and b >= 0:
+    print(" 1 - Вычислить функцию G\n  2 - Вычислить функцию F\n 3 - Вычислить функцию Y\nДругое значение - Выход из программы")
+    num1 = float(input('Значение'))
 else:
-    print('Введите число')
-
-print(" 1 - Вычислить функцию G\n"
-      " 2 - Вычислить функцию F\n"
-      " 3 - Вычислить функцию Y\n"
-      " Другое значение - Выход из программы")
-num1 = float(input('Значение'))
+    print('Неверное значение.Выход.')
 if num1 == 1:
     G = (9 * (20 * (b ** 2) - 31 * a * b + 12 * (b ** 2))) / (10 * (a ** 2) - 17 * a * b + 6 * (b ** 2))
     print('A = {}.\nB = {}.\nРезультат: {}'.format(a,b,G))
@@ -36,3 +26,4 @@ if element:
         element = float(element)
     except ValueError:
         print("Exit")
+
