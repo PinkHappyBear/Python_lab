397b9940bdb735102fdb26ccac066fc07696d832
PinkHappyBear <31932838+PinkHappyBear@users.noreply.github.com>
2017-11-06 13:48:36 +0300

    Update lab01.py

--- lab01/lab01.py
+++ lab01/lab01.py
@@ -15,17 +15,3 @@ a = float(input("Введите a:"))
 b = float(input("Введите b:"))
 Y = - atan (2 * (a ** 2) + (a * b) - 3 * (b ** 2))
 print('A = {}.\nB = {}.\nРезультат: {}'.format(a,b,Y))
-
-print("Пожалуйста оцените нашу программу по шкале от 0 до 5")
-if element == 1:
-    print("Спасибо за отзыв")
-elif element == 2:
-    print("Спасибо за отзыв")
-elif element == 3:
-    print("Спасибо за отзыв")
-elif element == 4:
-    print("Спасибо за отзыв")
-elif element == 5
-    print("Спасибо за отзыв")
-else:
-    print("Всего доброго")
